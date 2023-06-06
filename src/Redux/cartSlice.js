e0160599c1af0f87998735267d786277676d28a8
Nikhil Singh <nikhilsingh884@outlook.com>
2023-06-06 09:56:00 +0530

    some changes

--- src/Redux/cartSlice.js
+++ src/Redux/cartSlice.js
@@ -1,8 +1,8 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
 createSlice({
-    name:"cart",
-    initialState:{
-        totalItem:10
+    name: "cart",
+    initialState: {
+        totalItem: 11
     }
 })
